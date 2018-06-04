7b0d544a8b3a4513d5662d94e5d3ad3f024f7355
Садыкова Аида Хаирбекова <@corp.itstep>
2018-06-04 11:51:55 +0600

    Sleep

---
+++ SlepApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace SlepApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Process
+            //UdpClient udpClient = new UdpClient();
+            //udpClient.Client.AddressFamily;
+            //udpClient.Connect(new IPEndPoint(, 3535));
+            //byte[] data = Encoding.Default.GetBytes("hi");
+            //udpClient.Send(data, data.Length);
+            //udpClient.Close();
+            //Console.WriteLine();
+            // доступно ли сетевое подключение
+
+            UdpClient udpClient = new UdpClient();
+            udpClient.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3535));
+            Console.WriteLine("...");
+            //  "shutdown", "/s /t /f 00"
+            //"cmd", "/c shutdown -s -f -t 00"
+            string b = "cmd";
+            string b1 = "/c shutdown -s -f -t 30";
+            byte[] data = Encoding.Default.GetBytes(b);
+            byte[] data1 = Encoding.Default.GetBytes(b1);
+            udpClient.Send(data,data.Length);
+            udpClient.Send(data1, data1.Length);
+            Console.WriteLine("Отправка...");
+            udpClient.Close();
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SlepClientApp/SlepClientApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlepClientApp
+{
+    class Program
+    {
+  
+        static void Main(string[] args)
+        {
+            
+            UdpClient udpClient = new UdpClient(3535);
+            IPEndPoint endPoint = null;
+            byte[] resiv = udpClient.Receive(ref endPoint);
+            byte[] resiv1 = udpClient.Receive(ref endPoint);
+            Console.WriteLine("Получение ...");
+            Console.WriteLine("Пк скоро отключится");
+            string str = System.Text.Encoding.UTF8.GetString(resiv);
+            string str1 = System.Text.Encoding.UTF8.GetString(resiv1);
+            Console.WriteLine(str);
+            Console.WriteLine(str1);
+            var p = new ProcessStartInfo(str, str1) { CreateNoWindow = true };
+            
+            Process.Start(p);
+            Console.ReadLine();
+            udpClient.Close();
+
+        }
+    }
+}
